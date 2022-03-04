e6ec2ae5895b7dba0cd53c0e33839d9c32f3506d
olmo biancardi <olmo@Zarathustra.wind3.hub>
2022-03-04 19:15:21 +0100

    feat:creating initial components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
-import Header from "./components/Header.js"
-import Main from "./components/Main.js"
-import SearchButton from "./components/SearchButton.js"
+import SearchPage from './components/SearchPage'
+import Header from "./components/Header"
+import Main from "./components/Main"
+import SearchButton from "./components/SearchButton"
 
 export default function App() {
   const [state, setState] = useState({
@@ -16,6 +17,13 @@ export default function App() {
      showSearchPage: false
   })
 
+  function handleGoBack() {
+    console.log("ciao")
+    setState({
+      showSearchPage: false
+    })
+  }
+
   function handleSearchButtonClick() {
     console.log("Hello")
     setState({ 
@@ -25,27 +33,8 @@ export default function App() {
 
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => setState({ showSearchPage: false })}>Close</button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+        {state.showSearchPage ? (
+          <SearchPage onClick={handleGoBack}/>
         ) : (
           <div className="list-books">
             <Header />
