badf1d7926c98638f917c6a74860efc8a1879983
olmo <olmo1995@gmail.com>
2022-03-14 17:27:47 +0100

    feat:added search page

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,26 +1,83 @@
-import propTypes from "prop-types"
 import React from "react"
+import * as BooksAPI from '../BooksAPI'
+import { useState } from 'react'
+import Book from "./Book"
 
 export default function SearchPage(props) {
+
+    const [state, setState] = useState({
+      query: "",
+      result: []
+    })
+
+    function startSearch(event) {
+      const userInput = event.target.value
+      setState(prevState => {
+        return {
+          ...prevState,
+          query: userInput
+        }
+      })
+      BooksAPI.search(state.query)
+        .then((result) => {
+          setState(prevState => {
+            return {
+              ...prevState,
+              result
+            }
+          })
+          console.log(result) //first character is undefined here, why ?? (next characters are fine instead)
+        })
+    }
+
     return (
         <div className="search-books">
             <div className="search-books-bar">
               <button className="close-search" onClick={props.onClick}>Close</button>
               <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
+                {/* input field where user will type the book search, adding "value" 
+                to make it a controlled component */}
+                <input 
+                    type="text" 
+                    placeholder="Search by title or author" 
+                    onChange={startSearch}
+                    value={state.query}
+                />
 
               </div>
             </div>
             <div className="search-books-results">
-              <ol className="books-grid"></ol>
+
+              {/* creating dinamically the list of books based on user input search */}
+              <ol className="books-grid">
+                  <li>
+                    {state.result.map(book =>
+                        <Book 
+                          key={book.id}
+                          book={book}
+                          bookTitle={book.title} 
+                          bookAuthors={book.authors} 
+                          bookImage={book.imageLinks.thumbnail}
+                        />
+                    )}
+                  </li>
+              </ol>
+
+            </div>
+            <div className="search-terms">
+              <h4>You can search by typing the any of the following words:</h4>
+              'Android', 'Art', 'Artificial Intelligence', 'Astronomy', 'Austen', 'Baseball', 
+              'Basketball', 'Bhagat', 'Biography', 'Brief', 'Business', 'Camus', 'Cervantes', 
+              'Christie', 'Classics', 'Comics', 'Cook', 'Cricket', 'Cycling', 'Desai', 'Design', 
+              'Development', 'Digital Marketing', 'Drama', 'Drawing', 'Dumas', 'Education', 
+              'Everything', 'Fantasy', 'Film', 'Finance', 'First', 'Fitness', 'Football', 'Future', 
+              'Games', 'Gandhi', 'Homer', 'Horror', 'Hugo', 'Ibsen', 'Journey', 'Kafka', 'King', 
+              'Lahiri', 'Larsson', 'Learn', 'Literary Fiction', 'Make', 'Manage', 'Marquez', 'Money', 
+              'Mystery', 'Negotiate', 'Painting', 'Philosophy', 'Photography', 'Poetry', 'Production', 
+              'Programming', 'React', 'Redux', 'River', 'Robotics', 'Rowling', 'Satire', 
+              'Science Fiction', 'Shakespeare', 'Singh', 'Swimming', 'Tale', 'Thrun', 'Time', 'Tolstoy', 
+              'Travel', 'Ultimate', 'Virtual Reality', 'Web Development', 'iOS'
             </div>
         </div>
     )
